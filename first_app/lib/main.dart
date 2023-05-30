fa1b2e8981caefd12888293f98b6d669dcb56310
Hayato <hayato.314.nico@gmail.com>
2023-05-31 06:59:29 +0900

    講義35 クラスに分離する

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -1,24 +1,28 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MaterialApp(
       home: Scaffold(
-        body: Container(
-            decoration: const BoxDecoration(
-              gradient: LinearGradient(
-                colors: [Colors.deepPurple, Colors.greenAccent],
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight,
-              ),
-            ),
-            child: const Center(
-              child: Text(
-                'Hello World!!',
-                style: TextStyle(color: Colors.pinkAccent, fontSize: 56),
-              ),
-            )),
-      ),
-    ),
-  );
+    body: GradientContainer(),
+  )));
+}
+
+class GradientContainer extends StatelessWidget {
+  @override
+  Widget build(context) {
+    return Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Colors.deepPurple, Colors.greenAccent],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+        ),
+        child: const Center(
+          child: Text(
+            'Hello World!!',
+            style: TextStyle(color: Colors.black38, fontSize: 56),
+          ),
+        ));
+  }
 }
