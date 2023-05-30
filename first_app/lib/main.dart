17c15e9c4640444ee3313da60a715253781dceb4
Hayato <hayato.314.nico@gmail.com>
2023-05-31 06:19:43 +0900

    講義30　グラデーションカラー

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -2,11 +2,16 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!!'),
-        ),
+        body: Container(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                  colors: [Colors.deepPurple, Colors.greenAccent]),
+            ),
+            child: const Center(
+              child: Text('Hello World!!'),
+            )),
       ),
     ),
   );
