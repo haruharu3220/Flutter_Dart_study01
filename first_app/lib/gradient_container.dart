0a27186e8545e9f677a02f21c88c745471c9e44a
Hayato <hayato.314.nico@gmail.com>
2023-05-31 07:42:25 +0900

    Widgetの分離

--- first_app/lib/gradient_container.dart
+++ first_app/lib/gradient_container.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:first_app/styled_text.dart';
 
 class GradientContainer extends StatelessWidget {
   const GradientContainer({super.key});
@@ -14,10 +15,7 @@ class GradientContainer extends StatelessWidget {
           ),
         ),
         child: const Center(
-          child: Text(
-            'Hello World!!',
-            style: TextStyle(color: Colors.black38, fontSize: 56),
-          ),
+          child: StyledText(),
         ));
   }
 }
