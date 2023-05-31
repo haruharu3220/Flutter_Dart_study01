b46747c0f963b8e61c09d23e6b5b15e2ef4b29ed
Hayato <hayato.314.nico@gmail.com>
2023-05-31 13:10:11 +0900

    講義39 変数を定義

--- first_app/lib/gradient_container.dart
+++ first_app/lib/gradient_container.dart
@@ -1,17 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:first_app/styled_text.dart';
 
+var startAlignment = Alignment.topLeft;
+var endAlignment = Alignment.bottomRight;
+
 class GradientContainer extends StatelessWidget {
   const GradientContainer({super.key});
 
   @override
   Widget build(context) {
     return Container(
-        decoration: const BoxDecoration(
+        decoration: BoxDecoration(
           gradient: LinearGradient(
-            colors: [Colors.deepPurple, Colors.greenAccent],
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight,
+            colors: const [Colors.deepPurple, Colors.greenAccent],
+            begin: startAlignment,
+            end: endAlignment,
           ),
         ),
         child: const Center(
