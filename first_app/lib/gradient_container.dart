8e84f562d15869dac09c0234e51b57af8342a5f1
Hayato <hayato.314.nico@gmail.com>
2023-06-12 01:54:19 +0900

    サイコロアプリ完成

--- first_app/lib/gradient_container.dart
+++ first_app/lib/gradient_container.dart
@@ -1,24 +1,19 @@
 import 'package:flutter/material.dart';
 
-//変数が変更されない事がわかっている場合はfinal型を推奨
-final startAlignment = Alignment.topLeft;
-final endAlignment = Alignment.bottomRight;
+import 'package:first_app/dice_roller.dart';
+
+const startAlignment = Alignment.topLeft;
+const endAlignment = Alignment.bottomRight;
 
 class GradientContainer extends StatelessWidget {
-  GradientContainer(this.color1, this.color2, {super.key});
+  const GradientContainer(this.color1, this.color2, {super.key});
 
-  GradientContainer.purple()
+  const GradientContainer.purple({super.key})
       : color1 = Colors.deepPurple,
         color2 = Colors.indigo;
 
   final Color color1;
   final Color color2;
-  var activeDiceImage = 'assets/images/dice-5.png';
-
-  rollDice() {
-    activeDiceImage = 'assets/images/dice-4.png';
-    print('Changing image...');
-  }
 
   @override
   Widget build(context) {
@@ -30,28 +25,31 @@ class GradientContainer extends StatelessWidget {
           end: endAlignment,
         ),
       ),
-      child: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Image.asset(
-              activeDiceImage,
-              width: 200,
-            ),
-            const SizedBox(height: 20),
-            TextButton(
-              onPressed: rollDice,
-              child: const Text('Roll Dice'),
-              style: TextButton.styleFrom(
-                  // padding: const EdgeInsets.only(
-                  //   top: 20,
-                  // ),
-                  foregroundColor: Colors.white,
-                  textStyle: const TextStyle()),
-            )
-          ],
-        ),
+      child: const Center(
+        child: DiceRoller(),
       ),
     );
   }
 }
+
+// class GradientContainer extends StatelessWidget {
+//   const GradientContainer({super.key, required this.colors});
+
+//   final List<Color> colors;
+
+//   @override
+//   Widget build(context) {
+//     return Container(
+//       decoration: BoxDecoration(
+//         gradient: LinearGradient(
+//           colors: colors,
+//           begin: startAlignment,
+//           end: endAlignment,
+//         ),
+//       ),
+//       child: const Center(
+//         child: StyledText('Hello World!'),
+//       ),
+//     );
+//   }
+// }
--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -3,11 +3,11 @@ import 'package:first_app/gradient_container.dart';
 
 void main() {
   runApp(
-    MaterialApp(
+    const MaterialApp(
       home: Scaffold(
         body: GradientContainer(
-          const Color.fromARGB(255, 33, 5, 109),
-          const Color.fromARGB(100, 100, 100, 0),
+          Color.fromARGB(255, 33, 5, 109),
+          Color.fromARGB(100, 100, 100, 0),
         ),
       ),
     ),
