e48155ee27006d243f6382c067bae40b0e600856
Hayato <hayato.314.nico@gmail.com>
2023-06-11 23:23:43 +0900

    講義46 サイコロとボタンの間隔を調整

--- first_app/lib/gradient_container.dart
+++ first_app/lib/gradient_container.dart
@@ -28,12 +28,23 @@ class GradientContainer extends StatelessWidget {
       ),
       child: Center(
         child: Column(
+          mainAxisSize: MainAxisSize.min,
           children: [
             Image.asset(
               'assets/images/dice-2.png',
               width: 200,
             ),
-            TextButton(onPressed: rollDice, child: const Text('Roll Dice'))
+            const SizedBox(height: 20),
+            TextButton(
+              onPressed: rollDice,
+              child: const Text('Roll Dice'),
+              style: TextButton.styleFrom(
+                  // padding: const EdgeInsets.only(
+                  //   top: 20,
+                  // ),
+                  foregroundColor: Colors.white,
+                  textStyle: const TextStyle()),
+            )
           ],
         ),
       ),
